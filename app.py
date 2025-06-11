771eec9246d6f49bcb61065f1e8133d761c6bb1d
Faiz5000 <144349334+Faiz5000@users.noreply.github.com>
2025-06-11 15:08:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='/templates')
 CORS(app)
 
 auth = HTTPBasicAuth()
